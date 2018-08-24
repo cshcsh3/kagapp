8621deb0ffbf092a9927ffd0ddf15e07dc56a3f7
shanhan <thecsh3@gmail.com>
2018-08-24 15:25:05 +0900

    Skeleton created

--- app/src/main/java/edu/sit/great/kagapp/MainActivity.java
+++ app/src/main/java/edu/sit/great/kagapp/MainActivity.java
@@ -1,13 +1,48 @@
 package edu.sit.great.kagapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageButton navigateButton;
+    ImageButton translateButton;
+    ImageButton shopButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navigateButton = findViewById(R.id.navigateButton);
+        translateButton = findViewById(R.id.translateButton);
+        shopButton = findViewById(R.id.shopButton);
+
+        navigateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), NavigateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        translateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), TranslateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        shopButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), ShopActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
