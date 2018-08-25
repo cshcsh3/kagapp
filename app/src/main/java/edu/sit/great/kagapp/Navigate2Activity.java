f6ed50ef1e5e6cc67aea0f9ec6a16c91058ca711
Jeremy Lim <31715204+jiaemi46@users.noreply.github.com>
2018-08-25 15:01:00 +0800

    Revert "commit screen 2 and 3"
    This reverts commit 6e7402184a34bc8fd1523670cfe837664073ef5b.

--- app/src/main/java/edu/sit/great/kagapp/Navigate2Activity.java
+++ app/src/main/java/edu/sit/great/kagapp/Navigate2Activity.java
@@ -1,223 +1,24 @@
 package edu.sit.great.kagapp;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorManager;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
-import android.util.SparseArray;
-import android.view.Surface;
-import android.view.SurfaceHolder;
-import android.view.SurfaceView;
-import android.view.View;
-import android.view.animation.Animation;
-import android.view.animation.RotateAnimation;
 import android.widget.Button;
-import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.TextView;
-
-import com.google.android.gms.vision.CameraSource;
-import com.google.android.gms.vision.Detector;
-import com.google.android.gms.vision.text.TextBlock;
-import com.google.android.gms.vision.text.TextRecognizer;
-
-import java.io.IOException;
-import java.util.ArrayList;
 
 
 public class Navigate2Activity extends AppCompatActivity {
 
-    private static final String TAG = "CompassActivity";
-
-    private Compass compass;
-    private ImageView arrowView;
-    private TextView locationTextView;
-    private ImageButton endNavigationButton;
-    private TextView headingToTextView;
-    private TextView dumpingTextView;
-
-    private float currentAzimuth;
-    SurfaceView cameraView;
-    CameraSource cameraSource;
-    final int RequestCameraPermissionID = 1001;
-
-    //Integer Array Containing the bearing/azimuth of the hardcoded route
-    Float[] routeArray = { 90f, 5.3f, 180.0f, 33.3f, 5f };
-    public Integer currentRouteStep = 0;
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case RequestCameraPermissionID: {
-                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                        return;
-                    }
-                    try {
-                        cameraSource.start(cameraView.getHolder());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-            break;
-        }
-    }
+    TextView locationTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigate2);
-        arrowView = (ImageView) findViewById(R.id.rotatingArrow);
-        setupCompass();
-
-        endNavigationButton = findViewById(R.id.endNavigationButton);
-        dumpingTextView = findViewById(R.id.dumpingTextView);
-        headingToTextView = findViewById(R.id.headingToTextView);
-        cameraView = (SurfaceView) findViewById(R.id.navigate_surface);
-
 
         locationTextView = findViewById(R.id.locationTextView);
         String location = getIntent().getStringExtra("location");
         locationTextView.setText(location);
 
-        endNavigationButton = findViewById(R.id.endNavigationButton);
-
-        endNavigationButton.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
-                nextNavigateStep();
-            }
-        });
-
-        TextRecognizer textRecognizer = new TextRecognizer.Builder(getApplicationContext()).build();
-        if (!textRecognizer.isOperational()) {
-            Log.w("MainActivity", "Detector dependencies are not yet available");
-        } else {
-
-            cameraSource = new CameraSource.Builder(getApplicationContext(), textRecognizer)
-                    .setFacing(CameraSource.CAMERA_FACING_BACK)
-                    .setRequestedPreviewSize(1280, 1024)
-                    .setRequestedFps(2.0f)
-                    .setAutoFocusEnabled(true)
-                    .build();
-            cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
-                @Override
-                public void surfaceCreated(SurfaceHolder surfaceHolder) {
-
-                    try {
-                        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-
-                            ActivityCompat.requestPermissions(Navigate2Activity.this,
-                                    new String[]{Manifest.permission.CAMERA},
-                                    RequestCameraPermissionID);
-                            return;
-                        }
-                        cameraSource.start(cameraView.getHolder());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-
-                @Override
-                public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
-
-                }
-
-                @Override
-                public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
-                    cameraSource.stop();
-                }
-            });
-
-            textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
-                @Override
-                public void release() {
-
-                }
-
-                @Override
-                public void receiveDetections(Detector.Detections<TextBlock> detections) {
-
-                }
-            });
-        }
-    }
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "start compass");
-        compass.start();
     }
 
-    @Override
-    protected void onPause() {
-        super.onPause();
-        compass.stop();
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        compass.start();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "stop compass");
-        compass.stop();
-    }
-
-    private void setupCompass() {
-        compass = new Compass(this);
-        Compass.CompassListener cl = new Compass.CompassListener() {
-
-            @Override
-            public void onNewAzimuth(float azimuth) {
-                //adjustArrow(azimuth);
-                adjustArrow(azimuth - routeArray[currentRouteStep]);
-            }
-        };
-        compass.setListener(cl);
-    }
-
-    private void adjustArrow(float azimuth) {
-        Log.d(TAG, "will set rotation from " + currentAzimuth + " to "
-                + azimuth);
-
-        Animation an = new RotateAnimation(-currentAzimuth, -azimuth,
-                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
-                0.5f);
-        currentAzimuth = azimuth;
-
-        an.setDuration(500);
-        an.setRepeatCount(0);
-        an.setFillAfter(true);
-
-        dumpingTextView.setText("Bearing: " + azimuth);
-
-        arrowView.startAnimation(an);
-    }
-
-    //------Function to execute when endNavigationButton clicked
-    //------------This function points to the next index azimuth stored in the routeArray (Gradually turn)
-    public void nextNavigateStep(){
-        //Check if its end of journey
-        Log.d("XIAOJEM", "Yo: " + (Integer) routeArray.length);
-        if(currentRouteStep == (routeArray.length - 1)){
-            //Trigger End of Journey Here
-            headingToTextView.setText("End of Journey");
-        } else {
-            currentRouteStep++;
-            Log.d("XIAOJEM", "Current step: " + currentRouteStep);
-
-            //adjustArrow(routeArray[currentRouteStep - 1]);
-        }
-    }
 }
